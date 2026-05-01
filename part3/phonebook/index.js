bbbc4020df38753edb9d573a2bad0c40d79d00e0
Kingkar Bhowmick <kingkarbhowmick2@gmail.com>
2026-05-01 11:48:53 +0200

    feat(fso): Checkpoint 3.1, add package.json for phonebook project with express dependency

---
+++ part3/phonebook/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const { request } = require('node:http')
+
+const app = express()
+
+const persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+
+]
+
+
+app.get('/', (request, response ) => {
+    response.send('<h1><a href="http://localhost:3001/api/persons">Person list</a><h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
