aa71b0c9867c584d59ac9c2be246c7fc79b0aaf7
Kingkar Bhowmick <kingkarbhowmick2@gmail.com>
2026-06-01 00:53:34 +0200

    feat: update server port configuration and correct API endpoint in request files

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -138,7 +138,7 @@ app.get('/api/persons/:id',(request, response) => {
 })
 
 
-const PORT = 3002
+const PORT = process.env.PORT || 3002
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
